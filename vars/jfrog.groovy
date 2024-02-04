e004d4bdc6ddbfe6cad3ef19ceab896f455f819f
Lokesh-anji <62313968+Lokesh-anji@users.noreply.github.com>
2024-02-04 14:09:15 +0530

    Create jfrog.groovy

---
+++ vars/jfrog.groovy
@@ -0,0 +1,13 @@
+def call() {
+   rtUpload (
+                serverId: 'Jfrog',
+                spec: '''{
+                      "files": [
+                        {
+                          "pattern": "*.jar",
+                           "target": "example-repo-local/build-files/"
+                        }
+                    ]
+                }'''
+              )
+         }
